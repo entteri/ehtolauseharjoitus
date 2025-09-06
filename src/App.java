26b5d0cb78909c9291ca61021b0583390a0b0cd6
Daniel Mononen <daniel.mononen@student.hamk.fi>
2025-09-06 08:15:11 +0300

    juu

--- src/App.java
+++ src/App.java
@@ -1,32 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int luku1 = 15;
-        int luku2 = 15;
-        int luku3 = 15;
 
-        if (luku1 == luku2 || luku2 == luku3) {
-            System.out.println("Luku1 on yhtä suuri kuin Luku2 TAI Luku2 on yhtä suuri kuin Luku3");
-        } else if (luku1 > luku2 && luku1 == luku3) {
-            System.out.println("Luku1 on suurempi kuin Luku2 JA luku1 on yhtä suuri kuin Luku3");
-        } else if (luku1 == luku2 && luku1 == luku3) {
-            System.out.println("Luvut ovat yhtä suuria");
-        } else {
-            if (luku1 > luku2) {
-                System.out.println("Luku1 on suurempi kuin luku2");
-            } else {
-                if (luku2 > luku3 ) {
-                    System.out.println("Luku2 on suurempi kuin luku3");
-                } else {
-                    ;
-                }
-            }
+        String nimi1 = "maija";
+        String nimi2 = "ma";
+        String nimi3 = "maija";
+
+        if (nimi1 == nimi2) {
+            System.out.println("Nimi1 ja nimi2 ovat samat.");
         }
-        if (luku1 == luku2) {
-            System.out.println("Luku1 ja luku2 ovat yhtä suuria");
-        } else if (luku2 == luku3) {
-            System.out.println("Luku2 ja luku3 ovat yhtä suuria");
-            ;
+
+        if (nimi1 != nimi2) {
+            System.out.println("Nimi1 ja nimi2 ovat eri nimet.");
         }
-        System.out.println("Ohjelma päättyi.");
+
+        if (nimi1 == nimi2) {
+            System.out.println("Nimi1 ja nimi2 ovat samat.");
+        } else if (nimi1 == nimi3) {
+            System.out.println("Nimi1 ja nimi3 ovat samat.");
+        }
+
     }
-}
+
+}   
